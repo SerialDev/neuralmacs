35b23de2fad8aa5cdffd90a1ebf5388a7e15e846
amariscal <amariscal@cloudflare.com>
2020-05-02 11:33:04 -0700

    [+] fn() forward propagation, sigmoid activation & perceptron

--- neuralmacs.el
+++ neuralmacs.el
@@ -1,13 +1,13 @@
-;;; neuralmacs.el --- Neuralmacs minor mode for Hy Repl support  -*- lexical-binding: t -*-
+;;; neuralmacs.el --- Neuralmacs neural net support for emacs  -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2018 Andres Mariscal
 
 ;; Author: Andres Mariscal <carlos.mariscal.melgar@gmail.com>
 ;; Created: 1 May 2018
 ;; Version: 0.0.1
-;; Keywords: hy languages repl
-;; URL: https://github.com/serialdev/neuralmacs-mode
-;; Package-Requires: ((emacs "24.3", hy-mode ))
+;; Keywords: neural networks
+;; URL: https://github.com/serialdev/neuralmacs
+;; Package-Requires: ((emacs "24.3" ))
 ;;; Commentary:
 ;; Neural Networks in emacs
 
@@ -48,6 +48,44 @@
   result
   )
 
+(cl-defstruct (perceptron
+	       (:constructor perceptron-create))
+	       (input 0.0)
+	       actual-output weights bias epochs )
+
+(setq test (perceptron-create :input (make-list 10 0)))
+
+(setf (perceptron-bias test) 2)
+
+test
+
+(defun nm-init (perceptron)
+  (setf (perceptron-bias perceptron) 0.0)
+  (setf (perceptron-weights perceptron) (make-list (length (perceptron-input perceptron)) 0) )
+  (dotimes (num (length (perceptron-input perceptron)))
+    (setf
+     (nth num (perceptron-weights perceptron)) (random* 1.0)
+     )	 )
+  perceptron
+  )
+
+(defun nm-sigmoid(x)
+  "Sigmoid Activation"
+  (/ 1.0 (+ 1.0 (exp (- x))))
+  )
+
+(defun nm-fwd-pass(perceptron)
+  (sigmoid( + (nm-dot
+	   (perceptron-weights perceptron)
+	   (perceptron-input perceptron))
+	      (perceptron-bias perceptron) ) )
+  )
+
+
+(nm-init test)
+test
+
+(nm-fwd-pass test)
 
 (provide 'neuralmacs)
 
